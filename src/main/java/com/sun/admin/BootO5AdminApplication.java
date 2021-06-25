5464f237e639288da16108e1f1fec6de65ed8120
sunzibin <sunzibin@sunline.cn>
2021-06-25 23:34:56 +0800

    第一次提交

---
+++ src/main/java/com/sun/admin/BootO5AdminApplication.java
@@ -0,0 +1,13 @@
+package com.sun.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootO5AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootO5AdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sun/admin/BootO5AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sun.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootO5AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
